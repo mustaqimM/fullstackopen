cfceb673917b77ccb291973fe8cb10f325f84be1
mustaqim <git@mustaqim.ml>
2021-12-14 07:19:45 +0200

    Finish part2/phonebook/2.11 - Set state from server with useEffect

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,35 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '098-7654321' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' },
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('add a person')
   const [newNumber, setNewNumber] = useState('add a number')
-  const [findPerson, setFindPerson] = useState("")
+  const [findPerson, setFindPerson] = useState('')
+
+  const hook = () => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then((response) => setPersons(response.data))
+  }
+
+  useEffect(hook, [])
 
   const addPerson = (event) => {
     event.preventDefault()
 
     const personObject = {
       name: newName,
-      number: newNumber
+      number: newNumber,
     }
 
-    if (persons.find(person => person.name === newName)) {
+    if (persons.find((person) => person.name === newName)) {
       alert(`${newName} is already added to the phonebook`)
       return
     }
 
     setPersons(persons.concat(personObject))
-    setNewName("")
-    setNewNumber("")
+    setNewName('')
+    setNewNumber('')
   }
 
   const handlePersonChange = (event) => {
@@ -44,9 +49,12 @@ const App = () => {
     setFindPerson(event.target.value)
   }
 
-  const personsToShow = findPerson === ""
-    ? persons
-    : persons.filter(person => person.name.toLowerCase().includes(findPerson))
+  const personsToShow =
+    findPerson === ''
+      ? persons
+      : persons.filter((person) =>
+        person.name.toLowerCase().includes(findPerson),
+      )
 
   return (
     <div>
@@ -56,9 +64,13 @@ const App = () => {
 
       <h3>Add a new</h3>
 
-      <PersonForm addPerson={addPerson}
-        newName={newName} handlePersonChange={handlePersonChange}
-        newNumber={newNumber} handleNumberChange={handleNumberChange} />
+      <PersonForm
+        addPerson={addPerson}
+        newName={newName}
+        handlePersonChange={handlePersonChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
 
       <h2>Numbers</h2>
       <Persons personsToShow={personsToShow} />
