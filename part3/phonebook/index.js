d9d735fc99d38ee71a0036eea3c8d5a2c0a67d7c
mustaqim <git@mustaqim.ml>
2021-12-22 12:58:53 +0200

    Finish part3/phonebook/3.[7-8] - Add morgan middleware

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,21 +1,24 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:', request.path)
-  console.log('Body', request.body)
-  console.log('---')
-  next()
-}
-app.use(requestLogger)
-
-const unknownEndpoint = (request, response, next) => {
-  response.status(404).send({ error: 'Unknown Endpoint' })
-}
-app.use(unknownEndpoint)
+morgan.token('body', (req, res) => {
+  if (req.method === 'POST') {
+    return JSON.stringify(req.body)
+  }
+})
+app.use(morgan((tokens, req, res) => {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms',
+    tokens.body(req, res)
+  ].join(' ')
+}))
 
 let persons = [
   {
@@ -105,6 +108,12 @@ app.post('/api/persons', (request, response) => {
   persons = persons.concat(person)
   response.json(person)
 })
+
+// Handle non-existant routes
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown Endpoint' })
+}
+app.use(unknownEndpoint)
 
 
 const PORT = 3001
