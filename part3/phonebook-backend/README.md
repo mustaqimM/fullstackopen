359d89ed4898302b341d172ea5b4f5b8a74cba67
mustaqim <git@mustaqim.ml>
2021-12-25 09:00:30 +0200

    Finish part3/phonebook/3.[10-11] - Deploy to heroku; Add static frontend

---
+++ part3/phonebook-backend/README.md
@@ -0,0 +1,1 @@
+https://boiling-savannah-64960.herokuapp.com/api/persons
--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 
 morgan.token('body', (req, res) => {
   if (req.method === 'POST') {
@@ -118,7 +119,7 @@ const unknownEndpoint = (request, response) => {
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Running server on port: ${PORT}`);
 })
