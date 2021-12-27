15efef5adc54017efca763eef13b01394ba00f77
mustaqim <git@mustaqim.ml>
2021-12-27 18:40:25 +0200

    Finish part3/phonebook/3.16 - Move error handling to middleware

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -126,6 +126,15 @@ const unknownEndpoint = (request, response) => {
 }
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformed id' })
+  }
+  next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
